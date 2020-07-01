e49d157ee9bee982d14fb9e21677e640b931e3f4
nikki9119 <a.p.nikhilkumar@gmail.com>
2020-07-01 20:12:36 +0530

    Completed Application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WikipediaService } from './wikipedia.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wsearch';
+  pages = [];
+  loading = false;
+  constructor(private wikipedia:WikipediaService){}
+  onTerm(term:string){
+    this.loading = true;
+    this.wikipedia.search(term).subscribe((response)=>{
+      this.pages = response;
+      this.loading = false;
+    });
+  }
 }
---
+++ src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit,EventEmitter,Output } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css']
+})
+export class SearchBarComponent implements OnInit {
+
+  term = '';
+  @Output() submitted = new EventEmitter<string>();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onFormSubmit(event:any){
+    event.preventDefault();
+    this.submitted.emit(this.term);
+  }
+}
